89f91c3d34f205d82bf571f0f5aa04443ed1fd4f
Raju at LHG <raju@lhgraphics.com>
2023-08-16 22:04:43 +0600

    Added Approval controller

---
+++ Controller/LhgPayrollApprovalController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace KimaiPlugin\LhgPayrollBundle\Controller;
+
+// src/Controller/LhgPayrollApprovalController.php
+
+namespace KimaiPlugin\LhgPayrollBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use KimaiPlugin\LhgPayrollBundle\Entity\LhgPayrollApproval;
+use KimaiPlugin\LhgPayrollBundle\Form\LhgPayrollApprovalType;
+
+/**
+ * @Route(path="/admin/payroll-approval")
+ */
+class LhgPayrollApprovalController extends AbstractController
+{
+    /**
+     * @Route(path="", name="payroll", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(): Response
+    {
+        $approvals = $this->getDoctrine()->getRepository(LhgPayrollApproval::class)->findAll();
+
+        return $this->render('@LhgPayroll/approval/index.html.twig', ['approvals' => $approvals]);
+    }
+
+    /**
+     * @Route("/approval/new", name="lhg_payroll_approval_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $approval = new LhgPayrollApproval();
+        $form = $this->createForm(LhgPayrollApprovalType::class, $approval);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($approval);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('lhg_payroll_approval_index');
+        }
+
+        return $this->render('@LhgPayroll/approval/new.html.twig', [
+            'approval' => $approval,
+            'form' => $form->createView(),
+        ]);
+    }
+
+     
+}
